718ab2d737d539d0ad841b53b7fbe1b67e235188
Richard Geniesse <richard.geniesse@gmail.com>
2024-11-27 08:00:20 -0800

    Initial commit

---
+++ 2023/day_1/calibrator.py
@@ -0,0 +1,17 @@
+with open('2023/day_1/input.txt', 'r') as file:
+    final_result = 0
+    for word in file:
+        # Set first in, last int and result back to 0 for the next word.
+        firstint = 0
+        lastint = 0
+        result = 0
+        for char in word:
+            if char.isdigit() and firstint == 0:
+                firstint = char
+                lastint = char
+            elif char.isdigit() and firstint != 0:
+                lastint = char
+        result = str(firstint) + str(lastint)
+        print (result)
+        final_result = final_result + int(result)
+    print (final_result)
